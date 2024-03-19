3071119520499934d41e5f318a3e57b1660e45b5
HassanAdel12 <hassan.adel.hussein@gmail.com>
2024-03-19 22:59:51 +0200

    update project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",redirectTo:"Home",pathMatch:"full"},
+    {path:"Login",component:LoginComponent },
+    {path:"Register",component:RegisterComponent},
+
+];
